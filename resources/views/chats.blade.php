d14613cd93f59b1612035c3b45787e35fc248d7e
Josue Plaza <plazajosue2@gmail.com>
2017-02-26 14:43:19 -0600

    fixed chat responsiveness

--- resources/views/chats.blade.php
+++ resources/views/chats.blade.php
@@ -5,10 +5,10 @@
 @section('content')
     <div class="chats container-fluid">
     	<div class="row chat" style="background: #cecece;">
-    		<div class="col-md-4 chat-img-holder">
+    		<div class="col-xs-4 col-sm-4 col-md-4 chat-img-holder">
     			<img src="img/Composite.png" class="chat-img">
     		</div>
-    		<div class="col-md-8">
+    		<div class="col-xs-8 col-sm-8 col-md-8">
     			<div class="row">
     				<div class="col-md-12 chat-event-title">
     					<h1>DEADMAU5 CONCERT</h1>
@@ -27,10 +27,10 @@
     		</div>
     	</div>
     	<div class="row chat">
-    		<div class="col-md-4 chat-img-holder">
+    		<div class="col-xs-4 col-sm-4 col-md-4 chat-img-holder">
     			<img src="img/Composite.png" class="chat-img">
     		</div>
-    		<div class="col-md-8">
+    		<div class="col-xs-8 col-sm-8 col-md-8">
     			<div class="row">
     				<div class="col-md-12 chat-event-title">
     					<h1>FARMER'S MARKET</h1>
@@ -49,10 +49,10 @@
     		</div>
     	</div>
     	<div class="row chat" style="background: #cecece;">
-    		<div class="col-md-4 chat-img-holder">
+    		<div class="col-xs-4 col-sm-4 col-md-4 chat-img-holder">
     			<img src="img/Composite.png" class="chat-img">
     		</div>
-    		<div class="col-md-8">
+    		<div class="col-xs-8 col-sm-8 col-md-8">
     			<div class="row">
     				<div class="col-md-12 chat-event-title">
     					<h1>BURGER UTOPIA FESTIVAL</h1>
